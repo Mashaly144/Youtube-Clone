ac4d0b851e233b4cca20560ffd24188757a47f64
Mashaly144 <117321633+Mashaly144@users.noreply.github.com>
2023-07-11 23:05:42 +0300

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -6,7 +6,7 @@ const options = {
   url: BASE_URL,
   params: { part: 'snippet', videoId: 'M7FIvfx5J10' },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': "b4462a0833msh99e6bde36d24bcap1727a9jsnd94363328df8",
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
